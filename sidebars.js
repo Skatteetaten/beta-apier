16d9677eebbc5a4f6bff3f0c52a44af468d86586
Anita Kalkvik <84386976+anitakalkvik@users.noreply.github.com>
2024-04-24 23:01:36 +0200

    Update sidebars.js
    fjernet Krav og betalinger fra venstremenyen

--- sidebars.js
+++ sidebars.js
@@ -3,21 +3,6 @@ const sidebars = {
   docs: ['index',
     {
       type: 'category',
-      label: 'Krav og Betalinger',
-      link: { type: 'doc', id: 'kravogbetalinger/index' },
-      items: [{
-        type: 'link',
-        label: 'Krav og betalinger API',
-        href: 'https://skatteetaten.github.io/api-dokumentasjon/api/kravogbetalinger'
-      }, 'kravogbetalinger/tilgang',
-        'kravogbetalinger/feltbeskrivelser',
-        'kravogbetalinger/skisser',
-        'kravogbetalinger/referansedata',
-        'kravogbetalinger/faq']
-    },
-
-    {
-      type: 'category',
       label: 'Innkrevingsoppdrag',
       link: { type: 'doc', id: 'innkrevingsoppdrag/index' },
       items: [{
