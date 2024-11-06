c5a3c41ecede45d39394f0a450289087dd83b715
emilurnes-skatt <emil.urnes@skatteetaten.no>
2024-11-06 13:12:15 +0100

    FIUT-5339 bump swaggerhub

--- sidebars.js
+++ sidebars.js
@@ -34,7 +34,7 @@ const sidebars = {
         {
           type: 'link',
           label: 'API-dokumentasjon',
-          href: 'https://app.swaggerhub.com/apis/skatteetaten/utleggsbegjaering-app/0.0.1'
+          href: 'https://app.swaggerhub.com/apis/skatteetaten/utleggsbegjaering-app/0.0.2'
         },
         {
           type: 'category',
