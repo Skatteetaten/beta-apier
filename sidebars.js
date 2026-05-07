e14f2eb8a8c0499c29207665d9306cdb126b1755
Jan Morten Sørensen <JanMorten.Sorensen@skatteetaten.no>
2026-05-07 10:39:07 +0200

    Fjernet dokumentasjon for innkrevingsoppdrag da denne er flyttet til api-dokumentasjon

--- sidebars.js
+++ sidebars.js
@@ -3,31 +3,6 @@ const sidebars = {
   docs: ['index',
     {
       type: 'category',
-      label: 'Innkrevingsoppdrag',
-      link: { type: 'doc', id: 'innkrevingsoppdrag/index' },
-      items: [{
-        type: 'link',
-        label: 'API-dokumentasjon',
-        href: 'https://app.swaggerhub.com/apis-docs/skatteetaten/oppdragsinnkreving-api/'
-      },
-        'innkrevingsoppdrag/kom-i-gang',
-        'innkrevingsoppdrag/anbefalinger-for-bruk',
-        'innkrevingsoppdrag/beskrivelse-av-tjenester',
-        'innkrevingsoppdrag/funksjonell-beskrivelse',
-        'innkrevingsoppdrag/felles-valideringsregler',
-        {
-          type: 'category',
-          label: 'Oppdragsgiverspesifikke valideringsregler',
-          items: [
-            'innkrevingsoppdrag/oppdragsgiverspesifikke-valideringsregler/bronnoysundregistrene',
-            'innkrevingsoppdrag/oppdragsgiverspesifikke-valideringsregler/nav'
-          ]
-        }
-      ]
-    },
-
-    {
-      type: 'category',
       label: 'Utleggsbegjæring og prøving',
       link: { type: 'doc', id: 'utleggsbegjaering/index' },
       items: []
